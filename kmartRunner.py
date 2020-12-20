026f79ca4c3a9dc38bab47d7f752ef733dba0cb6
Hashim <hashimmufti1999@gmail.com>
2020-12-20 12:31:06 +1000

    Project cleanup/adding testing

---
+++ kmartRunner.py
@@ -0,0 +1,28 @@
+def runFromFile(inputFile):
+    with open(inputFile, "r") as iFile:
+        for iLine in iFile:
+            numberArray = iLine.rstrip().split(' ')
+            findLongestLength(numberArray)
+    return True
+
+def findLongestLength(inputArray):
+    maxLengthArray = []
+    tempArray = []
+    for number in inputArray:
+        # Base case (first run)
+        if len(tempArray) < 1:
+            tempArray.append(number)
+        # Case where number is greater end of array
+        elif int(number) > int(tempArray[-1]):
+            tempArray.append(number)
+        # Case where number is smaller than end of array
+        else:
+            if len(tempArray) > len(maxLengthArray):
+                maxLengthArray = tempArray
+            tempArray = []
+            tempArray.append(number)
+    # Compare last array in memory to current max array
+    if len(tempArray) > len(maxLengthArray):
+        maxLengthArray = tempArray
+    return maxLengthArray
+    
---
+++ test_FileRunner.py
@@ -0,0 +1,56 @@
+from kmartRunner import *
+import pytest
+
+def fileLineCount(file1, file2):
+    with open(file1, "r") as iFile, open(file2, "r") as oFile:
+        assert int(sum(1 for _ in iFile)) == int(sum(1 for _ in oFile))
+        
+def fileCompare(file1, file2):
+    with open(file1, "r") as iFile, open(file2, "r") as oFile:
+        testCaseNumber = 1
+        for iLine, oLine in zip(iFile, oFile):
+            print("For testcase ", testCaseNumber)
+            numberArray = iLine.rstrip().split(' ')
+            assert findLongestLength(numberArray) == oLine.rstrip().split(' ')
+            testCaseNumber += 1
+
+def test_github_files():
+    inputFile = "testFiles/realTestCasesInput.txt"
+    inputFile2 = "testFiles/realTestCasesOutput.txt"
+    fileLineCount(inputFile, inputFile2)
+    fileCompare(inputFile, inputFile2)
+
+def test_local_files():
+    inputFile = "testFiles/testCasesSimpleInput.txt"
+    inputFile2 = "testFiles/testCasesSimpleOutput.txt"
+    fileLineCount(inputFile, inputFile2)
+    fileCompare(inputFile, inputFile2)
+    
+def test_empty():
+    emptyArray = []
+    expectedEmptyArray = []
+    assert expectedEmptyArray == findLongestLength(emptyArray)
+    
+def test_all_zeroes():
+    zeroArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    assert [0] == findLongestLength(zeroArray)
+    
+def test_decreasing_sequence():
+    decreasingArray = [5,4,3,2,1]
+    assert [decreasingArray[0]] == findLongestLength(decreasingArray)
+    
+def test_increasing_sequence():
+    increasingArray = [1,2,3,4,5,6,7]
+    assert increasingArray == findLongestLength(increasingArray)
+
+def test_negative_sequence():
+    negativeArray = [-11,-10,-9,-8,-7,-6]
+    assert negativeArray == findLongestLength(negativeArray)
+
+def test_negative_positive_sequence():
+    mixedArray = [-12,11,200,-5,15,300,20]
+    assert [-12,11,200] == findLongestLength(mixedArray)
+
+def test_string():
+    with pytest.raises(ValueError):
+        findLongestLength('abcdefghijk')
